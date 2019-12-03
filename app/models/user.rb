b7d856a77f789d5d55a590e1ee74969b2e95b036
Nathan <nathancpt1@gmail.com>
2019-12-03 19:20:12 +0100

    [CREATE] add admin menu

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :accomodations, through: :favorites
 
-  validates :phone, presence: true, format: { with: /\A^((\+)33|0)[1-9](\d{2}){4}$*\z/i, message:  "Le format du numéro de téléphone est incorrect. Veuillez le vérifier." }
+  #validates :phone, format: { with: /\A^((\+)33|0)[1-9](\d{2}){4}$*\z/i, message:  "Le format du numéro de téléphone est incorrect. Veuillez le vérifier." }
 end
