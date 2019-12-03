0b9d7d0a1117202f8a673ae1b0a6878398f9d141
JamesElg <james.elangue19@gmail.com>
2019-12-03 11:09:37 +0100

    (CREATE) controller admin et view dashboard admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   resources :offers
   resources :companies
   resources :users
+
+  namespace :admin do
+    root to: 'dashboard#index'
+    resources :users, :accomodations, :offers, :companies, :users, :cities, :countries, :heatings, :join_table_secondaries, :join_table_tertiaries, :operation_types, :road_types, :secondary_criteria, :tertiary_criteria, :type_of_properties
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
