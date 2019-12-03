81eb3d6f6307a40e91b3534b0248e8be59e01ab1
Dieuyam <yamdjeuyannick@gmail.com>
2019-12-03 18:34:48 +0100

    Add Longitude Latitude to Accomadation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  before_action :configure_devise_parameters, if: :devise_controller?
+
+  def configure_devise_parameters
+    devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :first_name, :last_name, :phone, :address, :password, :password_confirmation, :current_password)}
+  end
 end
