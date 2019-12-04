89eedb761a79d8dd0df69ef4bf02741ec70bf465
JamesElg <james.elangue19@gmail.com>
2019-12-04 19:08:33 +0100

    (UPDATE) modification des erreurs sur migrations database && update seed avec accomodations

--- db/migrate/20191202180911_add_operation_type_to_accomodation.rb
+++ db/migrate/20191202180911_add_operation_type_to_accomodation.rb
@@ -1,5 +1,5 @@
 class AddOperationTypeToAccomodation < ActiveRecord::Migration[5.2]
   def change
-    add_reference :accomodations, :operation_types, foreign_key: true
+    add_reference :accomodations, :operation_type, foreign_key: true
   end
 end
