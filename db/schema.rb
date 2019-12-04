8f9ef4eb6579926859fc39c134e23af0b386cc18
JamesElg <james.elangue19@gmail.com>
2019-12-04 11:33:29 +0100

    (CREATE) active storage controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_170811) do
+ActiveRecord::Schema.define(version: 2019_12_04_100445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,27 @@ ActiveRecord::Schema.define(version: 2019_12_03_170811) do
     t.index ["operation_types_id"], name: "index_accomodations_on_operation_types_id"
     t.index ["road_types_id"], name: "index_accomodations_on_road_types_id"
     t.index ["type_of_property_id"], name: "index_accomodations_on_type_of_property_id"
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "cities", force: :cascade do |t|
@@ -168,6 +189,7 @@ ActiveRecord::Schema.define(version: 2019_12_03_170811) do
   add_foreign_key "accomodations", "operation_types", column: "operation_types_id"
   add_foreign_key "accomodations", "road_types", column: "road_types_id"
   add_foreign_key "accomodations", "type_of_properties"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "favorites", "accomodations"
   add_foreign_key "favorites", "users"
   add_foreign_key "join_table_secondaries", "accomodations"
