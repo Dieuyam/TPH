10b6b75e20922d2700a8a424cd81ff8e59ec6c00
JamesElg <james.elangue19@gmail.com>
2019-12-04 16:19:53 +0100

    (UPDATE) mise à jour schema suite à db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_12_04_140739) do
-=======
-ActiveRecord::Schema.define(version: 2019_12_04_100445) do
->>>>>>> dev
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +37,6 @@ ActiveRecord::Schema.define(version: 2019_12_04_100445) do
     t.float "longitude"
     t.float "latitude"
     t.text "title"
-
     t.index ["cities_id"], name: "index_accomodations_on_cities_id"
     t.index ["countries_id"], name: "index_accomodations_on_countries_id"
     t.index ["heating_id"], name: "index_accomodations_on_heating_id"
