0f1bd98157c637ff168c9da1ba88dabcce11d4ca
Nathan <nathancpt1@gmail.com>
2019-12-11 15:50:10 +0100

    [CREATE] security for nil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,22 @@ module ApplicationHelper
 			return instance
 		end
 	end
+
+	def if_nil_sort_one(instance)
+		if instance == nil
+			return 1
+		else
+			return instance
+		end
+	end
+
+	def if_nil_sort_false(instance)
+		if instance == nil
+			return false
+		else
+			return instance
+		end
+	end
 	def admin_to_check_icon(is_admin)
 		if is_admin == true
 			return render inline: '<i class="fas fa-check"></i>'
