9c2fa2c994b2866ab11c219574bce5bf086d4ca6
Nathan <nathancpt1@gmail.com>
2019-12-12 18:38:30 +0100

    [CREATE] Just 470 city 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -89,7 +89,7 @@ session = GoogleDrive::Session.from_service_account_key("config.json")
 ws = session.spreadsheet_by_key("1NxO5lRZIhqkrq2cG3N3pRaGXUHKOT8VjQO-dHMNM82E").worksheets[1]
 init_first_case = ws.rows.size + 1
 init_first_case.times do |i|
-	if ws[i+1, 1] != nil && cities_array.include? ws[i+1, 1]
+	if ws[i+1, 1] != nil && cities_array.include? ws[i+1, 1].downcase.tr(" -", "").tr("é", "e")
 		City.create(:name => ws[i+1, 1], :zipcode => ws[i+1, 2])
 	end
 	puts i
